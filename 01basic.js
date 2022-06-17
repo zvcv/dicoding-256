6167f9f7e2458047c46ee977be1dc146a8825d9c
Ulinnuha <76893088+zvcv@users.noreply.github.com>
2022-06-17 10:13:37 +0700

    let const

--- 01basic.js
+++ 01basic.js
@@ -6,3 +6,14 @@ console.log("uyeah");
 
 // Pada versi ECMAScript 2015 (ES6) dikenalkan deklarasi variabel dengan let dan const untuk menggantikan var yang dinilai kontroversial dan rawan menimbulkan bug
 
+let z;
+z = 'ngannou' + ' ' + 'ugimba';
+
+console.log(z);
+
+const x = 100;
+console.log(x);
+x = 200;
+console.log(x)
+
+/* TypeError: Assignment to constant variable. */
