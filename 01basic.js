6eeff3b1ff29e279b3882b9ee2bbf9bb8763f4d4
Ulinnuha <ulinnuha@dr.com>
2022-06-17 11:33:09 +0700

    if else

--- 01basic.js
+++ 01basic.js
@@ -25,3 +25,24 @@ const aNumber = 10
 
 console.log(aString == aNumber) // true, karena nilainya sama-sama 10
 console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
+
+let language = "French";
+let greeting = "Selamat Pagi"
+
+if (language === "English") {
+    greeting = "Good Morning!";
+} else if (language === "French") {
+    greeting = "Bonjour!"
+} else if (language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+}
+
+console.log(greeting);
+
+
+// simple if else
+// condition ? true expression : false expression
+
+const isMember = false;
+const discount = isMember ? 0.1 : 0;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
