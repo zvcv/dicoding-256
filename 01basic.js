9cf3a249435abbd52a48f569236ae981e9cdef3f
Ulinnuha <ulinnuha@dr.com>
2022-06-17 11:05:21 +0700

    datatypes

--- 01basic.js
+++ 01basic.js
@@ -13,7 +13,15 @@ console.log(z);
 
 const x = 100;
 console.log(x);
-x = 200;
-console.log(x)
+// x = 200;
+// console.log(x)
 
 /* TypeError: Assignment to constant variable. */
+
+console.log(typeof (x));
+
+const aString = '10';
+const aNumber = 10
+
+console.log(aString == aNumber) // true, karena nilainya sama-sama 10
+console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
