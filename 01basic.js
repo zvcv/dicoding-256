24fba183bdec7e9e47a69db121a815bb5aff115c
Ulinnuha <76893088+zvcv@users.noreply.github.com>
2022-06-17 09:56:38 +0700

    ngannou

--- 01basic.js
+++ 01basic.js
@@ -1,1 +1,8 @@
 console.log("uyeah");
+
+// this is a comment
+
+// var sering dianggap menghasilkan banyak bug
+
+// Pada versi ECMAScript 2015 (ES6) dikenalkan deklarasi variabel dengan let dan const untuk menggantikan var yang dinilai kontroversial dan rawan menimbulkan bug
+
