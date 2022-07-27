e939b084b4919b001f287958434bf99253b67c8e
Ulinnuha <ulinnuha@dr.com>
2022-07-27 09:24:43 +0700

    yo

--- 01basic.js
+++ 01basic.js
@@ -103,3 +103,30 @@ console.log(isMarried);
 module.exports = {
     firstName, lastName, age, isMarried,
 };
+
+
+/**
+ * TODO:
+ * Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array dengan ketentuan:
+ *   - Array tersebut menampung bilangan genap dari 1 hingga 100
+ *
+ * Catatan:
+ *   - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada array.
+ */
+
+// TODO
+let evenNumber = [];
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        evenNumber.push(i);
+    }
+}
+
+console.log(evenNumber);
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+// module.exports = evenNumber;
